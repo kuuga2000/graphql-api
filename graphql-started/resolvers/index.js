c8b411a832fda3d8180564cd55d97017541c1f54
ryuki.servaiv@gmail.com <ryuki.servaiv@gmail.com>
2023-12-11 21:28:11 +0700

    graphql-started - nested schema

--- graphql-started/resolvers/index.js
+++ graphql-started/resolvers/index.js
@@ -2,8 +2,32 @@ export const resolvers = {
     Query: {
         apiStatus: () => {
             return {
-                status: "OKxx"
+                status: "OK"
+            }
+        },
+        getUser: () => {
+            return {
+                status: "OK",
+                user_data: {
+                    firstName: "John",
+                    lastName: "Doe",
+                    email: "john@example.com"
+                }
             }
         }
-    }
+    },
+    // Mutation: {
+    //     createUser: (_, { input }) => {
+    //         return input;
+    //     }
+    // },
+    Mutation: {
+        createUser: (parent, args, context, info) => {
+            return {
+                firstName: args.input.firstName,
+                lastName: args.input.lastName,
+                email: args.input.email,
+            }
+        }
+    },
 }
