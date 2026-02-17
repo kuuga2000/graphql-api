1778b4e596a8630c7e7138b9cb92ceb0a4bd78c5
kuuga2000 <kuuga@rider.com>
2026-02-18 00:10:27 +0700

    graphql-slardar mutation

--- graphql-slardar/index.ts
+++ graphql-slardar/index.ts
@@ -36,12 +36,37 @@ const resolvers = {
       return db.authors.find((a) => a.id === parent.author_id)
     },
     game(parent: any) {
-      return db.games.find((g)=> g.id ===parent.game_id)
+      return db.games.find((g) => g.id === parent.game_id)
     }
   },
   Author: {
     reviews(parent: any) {
       return db.reviews.filter((r) => r.author_id === parent.id)
+    }
+  },
+  Mutation: {
+    addGame(_: any, args: any) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString()
+      }
+      db.games.push(game);
+      return game;
+    },
+    deleteGame(_: any, args: { id: string }) {
+      db.games = db.games.filter((g) => g.id !== args.id)
+      return db.games;
+    },
+    updateGame(_: any, args: any) {
+      db.games = db.games.map((g) => {
+        if (g.id === args.id) {
+          return { ...g, ...args.edits }
+        }
+
+        return g;
+      });
+
+      return db.games.find((g) => g.id === args.id)
     }
   }
 };
